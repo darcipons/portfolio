af72cb1c8565e16af986be624e0d858594fa833f
darcipons <darcimarie33@gmail.com>
2020-04-20 00:53:12 -0400

    added project pictures and projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,55 +1,121 @@
 import React from 'react';
-import { Card, CardDeck } from 'react-bootstrap'
+import { Card, ListGroup, ListGroupItem, CardColumns } from 'react-bootstrap'
 import './Projects.css'
+import Hangry from '../images/hangry.png'
+import Dog from '../images/dog.png'
+import HappyDog from '../images/happydog.png'
+import FoodHouse from '../images/foodhouse.png'
+import Todo from '../images/todo_app.png'
+import LoveablePaws from '../images/paws.png'
 
 const Projects = () => {
   return (
     <div className='project-container'>
       <div className='project-title'>Projects</div>
       <hr></hr>
-      
-      <CardDeck>
+
+      <CardColumns>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Dog} />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Random Dog Picture</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This content is a little bit longer.
+              Created using: JavaScript and Random Dog API
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
+          <ListGroup horizontal className="list-group-flush">
+            <ListGroupItem>
+              <Card.Link href="https://github.com/darcipons/random_dog" target="_blank">GitHub</Card.Link>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Card.Link href="https://darcipons.github.io/random_dog/">Live Code</Card.Link>
+            </ListGroupItem>
+          </ListGroup>
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={Todo} />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Todo App</Card.Title>
             <Card.Text>
-              This card has supporting text below as a natural lead-in to additional
-              content.{' '}
+              Created using: Flutter
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
+          <ListGroup horizontal className="list-group-flush">
+            <ListGroupItem>
+              <Card.Link href="https://github.com/darcipons/todo_flutter" target="_blank">GitHub</Card.Link>
+            </ListGroupItem>
+          </ListGroup>
         </Card>
         <Card>
-          <Card.Img variant="top" src="holder.js/100px160" />
+          <Card.Img variant="top" src={HappyDog} />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Happy Dog</Card.Title>
             <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in to
-              additional content. This card has even longer content than the first to
-              show that equal height action.
+              Created using: Ruby on Rails and TheDogAPI
             </Card.Text>
           </Card.Body>
-          <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </Card.Footer>
+          <ListGroup horizontal className="list-group-flush">
+            <ListGroupItem>
+              <Card.Link href="https://github.com/darcipons/darci_edwin_jajuan_syed" target="_blank">GitHub</Card.Link>
+            </ListGroupItem>
+            <ListGroupItem>
+             <Card.Link href="https://happydog101.herokuapp.com" target="_blank">Live Site</Card.Link>
+            </ListGroupItem>
+          </ListGroup>
         </Card>
-      </CardDeck>
+        <Card>
+          <Card.Img variant="top" src={Hangry} />
+          <Card.Body>
+            <Card.Title>Hangry</Card.Title>
+            <Card.Text>
+              Created using: JavaScript, React, & Yelp API
+            </Card.Text>
+          </Card.Body>
+          <ListGroup horizontal className="list-group-flush">
+            <ListGroupItem>
+              <Card.Link href="#" target="_blank">GitHub</Card.Link>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Card.Link href="https://wyn-hangry.herokuapp.com" target="_blank">Live Code</Card.Link>
+            </ListGroupItem>
+          </ListGroup>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={FoodHouse} />
+          <Card.Body>
+            <Card.Title>Food House</Card.Title>
+            <Card.Text>
+              Created using: JavaScript and React
+            </Card.Text>
+          </Card.Body>
+          <ListGroup horizontal className="list-group-flush">
+            <ListGroupItem>
+              <Card.Link href="https://github.com/darcipons/heroku-food-house" target="_blank">GitHub</Card.Link>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Card.Link href="https://darci-food-house.herokuapp.com" target="_blank">Live Site</Card.Link>
+            </ListGroupItem>
+          </ListGroup>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={LoveablePaws} />
+          <Card.Body>
+            <Card.Title>Loveable Paws</Card.Title>
+            <Card.Text>
+              Created using: JavaScript, React, PetFinder API, and TheDogAPI
+            </Card.Text>
+          </Card.Body>
+          <ListGroup horizontal className="list-group-flush">
+            <ListGroupItem>
+              <Card.Link href="https://github.com/darcipons/pet_adoption_pt12" target="_blank">GitHub</Card.Link>
+            </ListGroupItem>
+            {/* <ListGroupItem>
+             <Card.Link href="https://happydog101.herokuapp.com" target="_blank">Live Site</Card.Link>
+            </ListGroupItem> */}
+          </ListGroup>
+        </Card>
+        
+      </CardColumns>
     </div>
   )
 }
