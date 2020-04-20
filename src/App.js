56f65a54208ac08cfd1729193d37d4d798e0321c
darcipons <darcimarie33@gmail.com>
2020-04-19 22:52:08 -0400

    added home and navbar component, navbar is placed and pink

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
     <div className="App">
-      <h1>Yurrr</h1>
+      <Navbar/>
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import './Navbar.css'
+
+const Navigation = () => {
+  return (
+    <Navbar expand="lg" className='navbar'>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto nav-links">
+        <Nav.Link href="#home" className='link'>Home</Nav.Link>
+        <Nav.Link href="#projects" className='link'>Projects</Nav.Link>
+        <Nav.Link href="#about" className='link'>About</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+ 
+  )
+}
+
+export default Navigation 
